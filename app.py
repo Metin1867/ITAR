b090bf1fb6ac78d1f41db094ab0c8be4eafa17a5
Metin1867 <64594898+Metin1867@users.noreply.github.com>
2023-11-13 17:20:29 +0100

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ app = Flask(__name__)
 # Route for the GitHub webhook
 @app.route('/git_update', methods=['POST'])
 def git_update():
-    repo = git.Repo("../ITAR")
+    repo = git.Repo("./ITAR")
     origin = repo.remotes.origin
     repo.create_head('master',
                      origin.refs.master).set_tracking_branch(origin.refs.master).checkout()
